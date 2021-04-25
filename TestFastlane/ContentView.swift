fdb2086517d5c391987ff1ce64f20ed86f62135e
Martin Sheng <martin_sheng@hotmail.com>
2021-04-25 21:34:12 +0200

    test

---
+++ TestFastlane/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestFastlane
+//
+//  Created by Yuangang Sheng on 2021/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestFastlane/TestFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneApp.swift
+//  TestFastlane
+//
+//  Created by Yuangang Sheng on 2021/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
